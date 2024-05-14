0a956aef951c67b08fa1d3bd49698417cfb8cf09
Dahf <flacezockt@gmail.com>
2024-05-14 23:03:38 +0200

    removed cors

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ const app = express();
 app.use(bodyParser.json({limit: '100mb'}));
 app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.json());
 
