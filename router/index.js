95e4ac88ed7893992f1c4ec6785ac784fd623fa7
Dahf <flacezockt@gmail.com>
2024-05-14 17:02:21 +0200

    changed

--- router/index.js
+++ router/index.js
@@ -102,26 +102,27 @@ router.get("/status", (request, response) => {
         ingredientsArray.map(async ing => await translateText(ing, "en"))
       );
 
-      // Bedingungen für jede übersetzte Zutat erstellen
-      const ingredientConditions = translatedIngredients.map(translatedIngredient => ({
-        name: { [Op.iLike]: '%' + translatedIngredient + '%' }
-      }));
-
-      console.log(ingredientConditions)
-
+      // Suche nach Mahlzeiten, die alle übersetzten Zutaten enthalten
       foundItems = await Meal.findAll({
         include: [{
           model: Ingredient,
-          required: !!translatedIngredients.length,
-        }, {
-          required: !!translatedIngredients.length,
-          model: Ingredient,
-          as: "tagFilter",
+          required: true,
           where: {
-            [Op.and]: ingredientConditions
+            name: {
+              [Op.iLike]: { [Op.any]: translatedIngredients.map(ing => '%' + ing + '%') }
+            }
           }
         }],
       });
+
+      // Nach Mahlzeiten filtern, die alle Zutaten enthalten
+      foundItems = foundItems.filter(meal => {
+        const ingredientNames = meal.Ingredients.map(ingredient => ingredient.name.toLowerCase());
+        return translatedIngredients.every(translatedIngredient => 
+          ingredientNames.some(name => name.includes(translatedIngredient.toLowerCase()))
+        );
+      });
+
     } else {
       foundItems = await Meal.findAll({
         include: [{
