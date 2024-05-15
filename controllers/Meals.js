45fc4a245defbcb2943938e19e7e8f3186f92246
Dahf <flacezockt@gmail.com>
2024-05-15 17:59:04 +0200

    console log api

--- controllers/Meals.js
+++ controllers/Meals.js
@@ -23,8 +23,9 @@ async function translateText(text, targetLang, sourceLang = 'auto') {
 
 export const createMeal = async(request, response) => {
     const { name, image, description, servingSize, calories, fat, carbohydrates, protein, fiber, sugar, sodium, ingredients } = request.body
+    console.log(request.body);
     try {
-      console.log(request.body);
+      
       const meal = await Meal.create({
         name,
         image,
