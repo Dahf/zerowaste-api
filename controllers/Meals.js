97402fc99083ab0492a980a2345fcf3ab0c4a99a
Dahf <flacezockt@gmail.com>
2024-05-17 10:50:39 +0200

    idk

--- controllers/Meals.js
+++ controllers/Meals.js
@@ -76,7 +76,7 @@ export const getMeal = async (req, res) => {
                         }
                     }
                 ],
-                group: ['Meal.id'],
+                //group: ['Meal.id'],
                 having: Sequelize.literal(`COUNT(DISTINCT "tagFilter"."id") = ${ingredientConditions.length}`),
             });
         } else {
