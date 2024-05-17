feadff2af1652788e0138985b951787dbfaeef06
Dahf <flacezockt@gmail.com>
2024-05-17 12:26:03 +0200

    asd

--- controllers/Meals.js
+++ controllers/Meals.js
@@ -56,12 +56,11 @@ export const getMeal = async (req, res) => {
                     required: !!translatedIngredients.length,
                     model: Ingredient,
                     as: "tagFilter",
-                    where: {       
-                        [Op.and]: {
-                            name: { [Op.iLike]: translatedIngredients[0] },
-                            name: { [Op.iLike]: translatedIngredients[1] },
-                        }
-                    } 
+                    where: {
+                        [Op.and]: translatedIngredients.map(ingredient => ({
+                            name: { [Op.iLike]: ingredient }
+                        }))
+                    }
                     
                     
                     /*where: {
