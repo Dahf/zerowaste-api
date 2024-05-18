9cbce5b582c056e0cb2f7c7c38dceb2b26a51e8c
Dahf <flacezockt@gmail.com>
2024-05-18 21:02:47 +0200

    y

--- router/index.js
+++ router/index.js
@@ -6,6 +6,24 @@ import { refreshToken } from "../controllers/RefreshToken.js";
 
 const router = express.Router();
 
+async function translateText(text, targetLang, sourceLang = 'auto') {
+   const response = await fetch("https://translate.silasbeckmann.de/translate", {
+       method: "POST",
+       body: JSON.stringify({
+           q: text,
+           source: sourceLang,
+           target: targetLang,
+           format: "text",
+           api_key: process.env.TRANSLATE_API_KEY
+
+       }),
+       headers: { "Content-Type": "application/json" }
+   });
+
+   const data = await response.json();
+   return data.translatedText;
+}
+
 router.get("/status", (request, response) => {
     const status = {
        "Status": "Running"
@@ -19,7 +37,7 @@ router.get('/token', refreshToken);
 router.delete('/logout', Logout);
 router.get('/meals', getMeal);
 router.get('/top-generic-name', async (req, res) => {
-   
+
    const ingredientsArray = req.query.ingredients.split(',').map(ing => ing.trim());
 
    const translatedIngredients = await Promise.all(
