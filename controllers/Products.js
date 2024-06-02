f496a18263df22f3679add96fc5122e2e5e1f79b
Dahf <flacezockt@gmail.com>
2024-06-02 22:11:02 +0200

    extended limit

--- controllers/Products.js
+++ controllers/Products.js
@@ -16,12 +16,13 @@ export const getProductByBarcode = async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 }
-export const searchProducts = async (searchQuery) => {
+export const searchProducts = async (searchQuery, limit = 1000) => {
     return await Product.findAll({
       where: {
         product_name: {
             [Op.iLike]: `%${searchQuery}%`
         }
-      }
+      },
+      limit: limit
     });
   };
