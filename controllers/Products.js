26b9f4cb41bb1177d15001fe7e4b4072f8f2206f
Dahf <flacezockt@gmail.com>
2024-06-02 21:26:25 +0200

    k

--- controllers/Products.js
+++ controllers/Products.js
@@ -2,11 +2,16 @@ import Product from "../models/Products.js";
 
 export const getProductByBarcode = async (req, res) => {
     const { barcode } = req.query;
-    const product = await Product.findOne({
-        where: {
-            _id: barcode
-        }
-    });
-
-    res.json(product);
+    try {
+        const product = await Product.findOne({
+            where: {
+                _id: barcode
+            }
+        });
+        res.json(product);
+    } catch (error) {
+        // Handle the error appropriately
+        console.error('Error fetching product by barcode:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
