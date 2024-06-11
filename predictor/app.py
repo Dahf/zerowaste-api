68fcf3d2632f422bf1040b9a6f36b73c7077b7ec
Dahf <flacezockt@gmail.com>
2024-06-11 17:42:52 +0200

    asd

--- predictor/app.py
+++ predictor/app.py
@@ -10,22 +10,36 @@ from scipy.ndimage import gaussian_filter
 
 app = Flask(__name__)
 
-def vorverarbeitung(image):
+def preprocess_image(image):
     image_np = np.array(image)
 
-    # Ensure image is converted to grayscale
+    # Konvertiere das Bild in Graustufen
     gray = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
-    gray = cv2.resize(gray, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
 
+    # Rauschunterdrückung mit Gaussian Blur
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    # Adaptive Thresholding für Binarisierung
+    gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+
+    # Morphologische Operationen zur Verbesserung der Textregionen
     kernel = np.ones((1, 1), np.uint8)
     gray = cv2.dilate(gray, kernel, iterations=1)
     gray = cv2.erode(gray, kernel, iterations=1)
 
-    gray = gray.astype(np.uint8)
+    # Deskewing (Entzerrung)
+    coords = np.column_stack(np.where(gray > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = gray.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    gray = cv2.warpAffine(gray, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
 
-    processed_gray = cv2.adaptiveThreshold(cv2.medianBlur(gray, 3), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
-    config = ("--psm 6")
-    return pytesseract.image_to_string(processed_gray, config=config, lang='eng')
+    return gray
 
 @app.route('/')
 def home():
@@ -47,13 +61,10 @@ def find_match(regex, text):
 def predict():
     image_data = request.data
     image = Image.open(BytesIO(image_data)).convert('RGB')  # Ensure image is in RGB mode
-    image = ImageOps.invert(image)
-    contrast = ImageEnhance.Contrast(image)
-    image = contrast.enhance(2)
-
-    result = vorverarbeitung(image)
-
-    return jsonify({"matches": result})
+    processed_image = preprocess_image(image)
+    result_text = pytesseract.image_to_string(processed_image, config='--psm 6', lang='eng')
+    
+    return jsonify({"result": result_text})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
